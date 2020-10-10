acf68be0a09b5fd3dcdbcc8428d9f7d65bbf4cf6
Chelsey <cswilik@gmail.com>
2020-10-10 22:42:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,8 @@ end
 
 def monopoly_with_second_tier
   new_hash = {:railroads => {
-    :pieces => 4
+    :pieces => 4, 
+    :rent_in_dollars => { }
   }
 }
   # you built in the previous method. Run the tests. They will fail, but
