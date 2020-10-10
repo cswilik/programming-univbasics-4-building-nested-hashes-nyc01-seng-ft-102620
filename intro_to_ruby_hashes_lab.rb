0c8bfb88a7f4aaf1503949a00ea62c8426391586
Chelsey <cswilik@gmail.com>
2020-10-10 22:45:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,9 @@ def monopoly_with_third_tier
   new_hash = {:railroads => {
     :pieces => 4, 
     :rent_in_dollars => {
-      :one_piece_owned => 25 },
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+    },
     :names => { }
   }
 }
